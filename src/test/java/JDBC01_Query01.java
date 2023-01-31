78620d91d2b05bb2220c831f8af2ff1bc80605da
SelmanKarakoc <selman_karakoc@yahoo.com>
2023-01-31 13:05:52 +0300

    first commit

---
+++ src/test/java/JDBC01_Query01.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+public class JDBC01_Query01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 1 - Ilgili Driver'i yuklemeliyiz. MySQL kullandigimizi bildiriyoruz.
+        // Driver'i bulamama ihtimaline karsi forName metodu icin ClassNotFoundException
+        // method signature'imiza axception olarak firlatmamizi istiyor.
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+    // 2 - Baglantiyi olusturmak icin username ve password girmeliyiz.
+    // Burada da bu username ve password'un yanlis olma ihtimaline karsi
+    // SQLException firlatmamizi istiyor.
+
+        Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/sys?serverTimezone=UTC", "root", "1234");
+
+    // 3 - SQL query'leri icin bir Statement objesi olusturup , javada SQL
+    // sorgularimiz icin bir alan acacagiz.
+
+        Statement st=con.createStatement();
+
+    // 4 - SQL query lerimizi yazip, çalışıtrabiliriz
+
+        ResultSet veri=st.executeQuery("select * from personel");
+
+    // 5 - Sonuçları görmek için iteration ile Set içerisindeki elemanları
+    // while döngüsü ile yazdırıyoruz
+
+        while (veri.next()){
+            System.out.println(veri.getInt(1)+" "+veri.getString(2)+" "+veri.getString(3)
+            +" "+veri.getInt(4)+" "+veri.getString(5));
+
+        }
+    // 6 - Oluşturulan bağlantıları kapatıyoruz
+
+        con.close();
+        st.close();
+        veri.close();
+    }
+}
